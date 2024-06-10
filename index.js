ef3ddeb6d6c0dc50c10aed6b531972d179bd7f6f
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-06-10 16:54:21 +0600

    forgotten to commit

---
+++ index.js
@@ -0,0 +1,149 @@
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const cors = require("cors");
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gvtp0gh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      const database = client.db("touristGuideDb");
+      const userCollection = database.collection("users");
+      const packageCollection = database.collection("packages");
+      const reviewsCollection = database.collection("reviews");
+      const wishListCollection = database.collection("wishList");
+      const paymentsCollection = database.collection("payments");
+
+
+      //   jwt related apis
+       app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+       })
+      
+      // middleware
+    const verifyJWT = (req, res, next) => {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    }
+    
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    }
+
+
+
+
+      //   our packages related apis
+       app.get("/ourPackages", async (req, res) => {
+      const result = await packageCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/ourPackages/:id",  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id};
+      const result = await packageCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/ourPackages", async (req, res) => {
+      const newItem = req.body;
+      const result = await packageCollection.insertOne(newItem);
+      res.send(result);
+    })
+
+    app.delete("/ourPackages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await packageCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+    //   wishlist related apis
+       app.get("/wishList", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await wishListCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post("/wishList", async (req, res) => {
+      const wishListItem = req.body;
+      const result = await wishListCollection.insertOne(wishListItem);
+      res.send(result);
+    });
+
+    app.delete("/wishList/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishListCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
