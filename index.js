13f4d53da86b520c5760210fca70849627cda94e
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-08-27 00:09:14 +0600

    almost complete

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const jwt = require("jsonwebtoken");
 const app = express();
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://assignment-12-tourist-guide.web.app", "https://assignment-12-tourist-guide.firebaseapp.com"],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
+
 
 
 const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
@@ -82,12 +89,18 @@ async function run() {
 
     //   ACCESS_TOKEN_SECRET
 
-      //   user related apis
+    //   user related apis
+    
+    
+    //! get all users
     app.get("/users", verifyJWT, verifyAdmin,  async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
 
+
+
+    //? get an user
 
     app.get("/users/:email", async (req, res) => {
       const email = req?.params?.email;
@@ -113,7 +126,7 @@ async function run() {
     });
 
 
-    // get all tourGuides
+    //? get all tourGuides
     app.get("/allTourGuides", async (req, res) => {
   try {
     const query = {  role: "tourGuide" };
@@ -153,6 +166,40 @@ async function run() {
       }
       const result = await userCollection.insertOne(user);
       res.send(result);
+    })
+
+    // let's use put method instead of post method
+    app.put("/user", async (req, res) => {
+      const user = req.body;
+     
+      const query = { email: user?.email };
+        const isExist = await userCollection.findOne(query)
+      
+      
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await userCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+       const options = {
+        upsert: true
+      }   
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+          
+        },
+      }
+      const result = await userCollection.updateOne(query, updateDoc, options)
+      res.send(result)
     })
     // i have to check admin and verifyJWT here
     app.patch("/users/admin/:id", verifyJWT, verifyAdmin,  async (req, res) => {
@@ -195,6 +242,7 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+
     
     app.put("/users", async (req, res) => {
   const user = req.body;
@@ -283,9 +331,15 @@ async function run() {
 
 
       //   our packages related apis
-       app.get("/ourPackages", async (req, res) => {
-      const result = await packageCollection.find().toArray();
+    app.get("/ourPackages", async (req, res) => {
+      const tourType = req?.query?.tourType;
+      
+      let query = {}
+      if (tourType && tourType !== 'null') query = { tourType }
+      
+      const result = await packageCollection.find(query).toArray();
       res.send(result);
+      
     });
     app.get("/ourPackages/:id",  async (req, res) => {
       const id = req.params.id;
@@ -333,7 +387,47 @@ async function run() {
       res.send(result);
     });
 
+
+    // tourist review or story telling section related apis
+
+    // get info from paymentscollection to post in story telling section, those who booked tours will see in story telling section, their they will tell their experience. for this i need that booked tour title, tour guide name and tour guide email, and the information of that user who booked that tour. there is a button in the front end share your experience. If that user click this button then a form will be shown. In that form i will have That tour package's title, tour guide name and tour guide email, and the information of that user who booked that tour and his or her review. Now i will save that review in review collection. After that i will get all reviews from review collection with all data, and i will show it in the homepage Story telling section. 
+    // 1. create an api for single completed tour details
+    // 2. now post that tour details in story reviewsCollection collection
+    // 3. get that tour details from story reviewsCollection collection
+
+    
+    app.get("/tour_story", async (req, res) => {
+      const result = await reviewsCollection.find().limit(10).toArray();
+      res.send(result);
+    })
+    
+    
+    
+     app.get("/tour_story/:id",  async (req, res) => {
+       const id = req.params.id;
+       console.log(id);
+       
+      const query = { _id: new ObjectId(id) };
+      const result = await reviewsCollection.findOne(query);
+       res.send(result);
+       
+    })
+
+    app.post("/tour_story", async (req, res) => {
+      const tour_story = req.body;
+      const result = await reviewsCollection.insertOne(tour_story);
+      res.send(result);
+    });
+
+
+
     //   payment related apis
+
+    
+  
+
+
+
     app.get("/payments", verifyJWT, async (req, res) => {
       const email = req?.query?.email;
        if ( email !== req?.decoded?.email  ) {
